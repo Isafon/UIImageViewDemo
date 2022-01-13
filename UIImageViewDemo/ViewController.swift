4ba5bb4a2f78f56fd0d92fafc350d1ed1222ee2e
Isa Fontana <isfon8@gmail.com>
2022-01-13 14:23:02 -0700

    Added Images

--- UIImageViewDemo/ViewController.swift
+++ UIImageViewDemo/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    let myImage = UIImage(named: "MMEGray")
+    
+    @IBOutlet weak var myImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myImageView.image = myImage
+        
     }
 
-
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        let myImage = UIImage(named: "MMEBlack")
+        myImageView.image = myImage
+    }
 }
 
